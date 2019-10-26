b67171d5ed62143952264783078558391cc2b33a
Alex Yarotsky <yarotsky.alex@gmail.com>
2019-10-26 18:11:22 +0300

    Format code according to PEP8

--- cart_pole.py
+++ cart_pole.py
@@ -3,16 +3,56 @@ import imageio
 from tqdm import tqdm
 
 from tf_agents.agents.dqn import dqn_agent
-from tf_agents.drivers import dynamic_step_driver
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
-from tf_agents.eval import metric_utils
-from tf_agents.metrics import tf_metrics
 from tf_agents.networks import q_network
 from tf_agents.policies import random_tf_policy
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.utils import common
+
+# https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
+def compute_avg_return(environment, policy, num_episodes=10):
+    total_return = 0.0
+
+    for _ in range(num_episodes):
+        time_step = environment.reset()
+        episode_return = 0.0
+
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+
+        total_return += episode_return
+
+    avg_return = total_return / num_episodes
+
+    return avg_return.numpy()[0]
+
+def collect_step(environment, policy, buffer):
+    time_step = environment.current_time_step()
+    action_step = policy.action(time_step)
+    next_time_step = environment.step(action_step.action)
+    buffer.add_batch(
+        trajectory.from_transition(time_step, action_step, next_time_step)
+    )
+
+# https://github.com/tensorflow/agents/blob/master/tf_agents/docs/python/tf_agents/drivers.md
+def collect_data(env, policy, buffer, steps):
+    for _ in range(steps):
+        collect_step(env, policy, buffer)
+
+def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
+    with imageio.get_writer(f'{filename}.mp4', fps=fps) as video:
+        for _ in tqdm(range(num_episodes), desc='Evaluating DQN'):
+            time_step = eval_env.reset()
+            video.append_data(eval_py_env.render())
+
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = eval_env.step(action_step.action)
+                video.append_data(eval_py_env.render())
 
 num_iterations = 40000
 initial_collect_steps = 1000
@@ -41,49 +81,13 @@ agent = dqn_agent.DqnAgent(train_env.time_step_spec(),
 
 agent.initialize()
 
-eval_policy = agent.policy
-collect_policy = agent.collect_policy
-
 random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
                                                 train_env.action_spec())
-
-# https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
-def compute_avg_return(environment, policy, num_episodes=10):
-  total_return = 0.0
-
-  for _ in range(num_episodes):
-    time_step = environment.reset()
-    episode_return = 0.0
-
-    while not time_step.is_last():
-      action_step = policy.action(time_step)
-      time_step = environment.step(action_step.action)
-      episode_return += time_step.reward
-
-    total_return += episode_return
-
-  avg_return = total_return / num_episodes
-
-  return avg_return.numpy()[0]
 
 replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
     data_spec=agent.collect_data_spec,
     batch_size=train_env.batch_size,
     max_length=replay_buffer_max_length)
-
-def collect_step(environment, policy, buffer):
-  time_step = environment.current_time_step()
-  action_step = policy.action(time_step)
-  next_time_step = environment.step(action_step.action)
-  traj = trajectory.from_transition(time_step, action_step, next_time_step)
-
-  # Add trajectory to the replay buffer
-  buffer.add_batch(traj)
-
-# https://github.com/tensorflow/agents/blob/master/tf_agents/docs/python/tf_agents/drivers.md
-def collect_data(env, policy, buffer, steps):
-  for _ in range(steps):
-    collect_step(env, policy, buffer)
 
 collect_data(train_env,
              random_policy,
@@ -101,23 +105,12 @@ iterator = iter(dataset)
 agent.train = common.function(agent.train)
 
 for _ in tqdm(range(num_iterations), desc='Training DQN'):
-  # Collect a few steps using collect_policy and save to the replay buffer.
-  for _ in range(collect_steps_per_iteration):
-    collect_step(train_env, agent.collect_policy, replay_buffer)
+    # Collect a few steps using collect_policy and save to the replay buffer.
+    for _ in range(collect_steps_per_iteration):
+        collect_step(train_env, agent.collect_policy, replay_buffer)
 
-  # Sample a batch of data from the buffer and update the agent's network.
-  experience, unused_info = next(iterator)
-  agent.train(experience)
-
-def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
-  with imageio.get_writer(f'{filename}.mp4', fps=fps) as video:
-    for _ in tqdm(range(num_episodes), desc='Evaluating DQN'):
-      time_step = eval_env.reset()
-      video.append_data(eval_py_env.render())
-
-      while not time_step.is_last():
-        action_step = policy.action(time_step)
-        time_step = eval_env.step(action_step.action)
-        video.append_data(eval_py_env.render())
+    # Sample a batch of data from the buffer and update the agent's network.
+    experience, unused_info = next(iterator)
+    agent.train(experience)
 
 create_policy_eval_video(agent.policy, 'trained-agent', num_eval_episodes)
